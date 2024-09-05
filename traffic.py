0d19ffca9e060b7961028fd7796ca4f60d59ca1a
M.Iqbal <muhammadiq19@gmail.com>
2024-09-05 21:41:36 +0700

    fix file

--- traffic.py
+++
@@ -1,6 +0,0 @@
-class TrafficJam:
-
-    def __init__(self):
-        self.light = 0
-        self.moderate = 0
-        self.heavy = 0
---
+++ yolov8/traffic.py
@@ -0,0 +1,6 @@
+class TrafficJam:
+
+    def __init__(self):
+        self.light = 0
+        self.moderate = 0
+        self.heavy = 0
